8a8f8c4791aa41000af31717e36ea0845f162b8d
AlexKhvostov <Lihach.ok@gmail.com>
2018-11-23 23:05:29 +0300

    work to bd MySQL. First task

---
+++ app_service/first_app_mysql.py
@@ -0,0 +1,115 @@
+from builtins import print
+
+import sqlalchemy #начните работу с этой библиотеки
+from datetime import date
+from sqlalchemy import create_engine
+from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
+from sqlalchemy.orm import mapper, sessionmaker
+
+
+#Создаем подключение к базе данных с использованием логина и пароля
+engine = create_engine("mysql://alex:123@localhost/Learn?host=localhost?port=3306")
+#открываем сессию  ( пока не понятно замем. В расках сессии идет работа с базой и пользователями.
+Session = sessionmaker(bind=engine)
+# Создаем объекат session класса Session() для общения с базой данных
+session = Session()
+
+
+''' привязываем таблицы к коду'''
+metadata = MetaData()
+''' таблица отделов '''
+dept_table = Table('dept', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50))
+)
+
+''' таблица работников '''
+workers_table = Table('workers', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('deptname', String(30)),
+    Column('fullname', String(30)),
+    Column('birthday', String(30)), # в будущем поменять на дату
+    Column('salary', Integer)
+)
+'''создаем описаные таблица , в метод встроена проверка наличия таких баз'''
+metadata.create_all(engine)
+
+
+''' создаем объекты класса Dept'''
+class Dept(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Dept('%s')>" % (self.name)
+
+''' создаем объекты класса Workers'''
+class Workers(object):
+    def __init__(self, deptname, fullname, birthday, salary):
+        self.deptname = deptname
+        self.fullname = fullname
+        self.birthday = birthday
+        self.salary = salary
+
+    def __repr__(self):
+        return "<Workers('%s','%s', '%s', '%s')>" % (self.deptname, self.fullname, self.birthday, self.salary)
+
+'''метода для слияния таблицы и класса '''
+mapper(Dept, dept_table)
+mapper(Workers, workers_table)
+
+
+
+
+def selectBases():
+    sBase = 0
+    while (sBase != 1) and (sBase != 2):
+        sBase = int(input("----------------------------------\n"
+                          "| Выберите базу данных для работы: \n"
+                          "| 1 - отделы.  \n"
+                          "| 2 - сотрудники \n"
+                          "| Ваш выбор : "))
+    return(sBase)
+
+
+def selectAct():
+    nAct=0
+    while (nAct != 1) and (nAct != 2) and (nAct != 3):
+        nAct = int(input("----------------------------------\n"
+                         "| Выберите действие для обработки: \n"
+                          "| 1 - добавить запись.  \n"
+                          "| 2 - удалить запись \n"
+                          "| 3 - просмотреть записи \n"
+                          "| Ваш выбор : "))
+    return(nAct)
+
+def useBases(n):
+    pass
+
+def addItem(base):
+    if base == 1:
+        print("-------------\n Добавляем запись в таблицу 'отделы'.")
+        deptname=input("Введите название отдела:")
+        item = Dept(deptname)
+    elif base == 2:
+        print("-------------\n Добавляем запись в таблицу 'сотрудники'.")
+        deptname = int(input("Выберите название отдела(число до 10): ")) # должны дергаться из базы . решить перебором через if.
+        fullname = input("Введите имя сотрудника: ")
+        birthday = input("Введите дату рождения: ")
+        salary = int(input("Ввдите зарплату сотрудника: "))
+        item = Workers(deptname, fullname, birthday, salary)
+    else:
+        print("введены неверные данные")
+
+    return item
+
+
+b=selectBases()
+a=selectAct()
+
+if a == 1:
+    ''' создание и добавление в сессию объектов классов  User и Dept'''
+    addeditem = addItem(b)
+    session.add(addeditem)
+    session.commit()
+    1
--- app_service/test.py
+++ app_service/test.py
@@ -5,24 +5,41 @@ from sqlalchemy import create_engine
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey
 from sqlalchemy.orm import mapper, sessionmaker
 
+#Создаем подключение к базе данных с использованием логина и пароля
 engine = create_engine("mysql://alex:123@localhost/Learn?host=localhost?port=3306")
+#открываем сессию  ( пока не понятно замем. В расках сессии идет работа с базой и пользователями.
 Session = sessionmaker(bind=engine)
+# Создаем объекат session класса Session() для общения с базой данных
 session = Session()
 
-conn = engine.connect()
-conn.execute("SELECT * FROM dept")
+# тестовые запросы к базе
+#conn = engine.connect()
+#conn.execute("SELECT * FROM dept")
 
-print("Версия SQLAlchemy:", sqlalchemy.__version__)  # посмотреть версию SQLALchemy
+#тестирование состояния модуля.
+#print("Версия SQLAlchemy:", sqlalchemy.__version__)  # посмотреть версию SQLALchemy
 
+
+''' привязываем таблицы к коду'''
 metadata = MetaData()
+''' таблица пользователей '''
 users_table = Table('users', metadata,
     Column('id', Integer, primary_key=True),
     Column('name', String(30)),
     Column('fullname', String(30)),
     Column('password', String(30))
 )
+''' таблица отделов '''
+dept_table = Table('dept', metadata,
+    Column('id', Integer, primary_key=True),
+    Column('name', String(50))
+)
+
+'''создаем описаные таблица , в метод встроена проверка наличия таких баз'''
 metadata.create_all(engine)
 
+'''Создаем классы для работы с таблицами в коде python'''
+''' User'''
 class User(object):
     def __init__(self, name, fullname, password):
         self.name = name
@@ -32,44 +49,66 @@ class User(object):
     def __repr__(self):
         return "<User('%s','%s', '%s')>" % (self.name, self.fullname, self.password)
 
+''' Dept'''
+class Dept(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return "<Dept('%s')>" % (self.name)
+
+
+'''метода для слияния таблицы и класса '''
 mapper(User, users_table)
+mapper(Dept, dept_table)
 # <Mapper at 0x...; User>
 
+''' создаем и проверяем объект типа :'''
+#User
+t_user = User("vasia", "Vasilij", "qweasdzxc")
+#Dept
+t_dept = Dept("developer")
 
-# Вывод параметров на экран для контроля и понимания.
-#print(mapper(User, users_table))
+#vasiaUser = User("vasia", "Vasiliy Pypkin", "vasia2000")
+#session.add(vasiaUser)
 
-user = User("Вася", "Василий", "qweasdzxc")
-vasiaUser = User("vasia", "Vasiliy Pypkin", "vasia2000")
-session.add(vasiaUser)
+''' создание и добавление в сессию объектов классов  User и Dept'''
+session.add(t_user) # добавление одной записи ( объектов )
+session.add(t_dept) # --//--//--
+session.add_all([Dept("cleaner"), Dept("logists")]) # добваление множества записей (объектов )
+session.add_all([User("kolia", "Cool Kolian[S.A.]","kolia$$$"), User("zina", "Zina Korzina", "zk18")]) # --//--
+#t_user.password = "-=VP2001=-"
 
-session.add_all([User("kolia", "Cool Kolian[S.A.]","kolia$$$"), User("zina", "Zina Korzina", "zk18")])
-vasiaUser.password = "-=VP2001=-"
+''' объект запроса '''
+#ourUser = session.query(User).filter_by(name="Вася").first()
 
-ourUser = session.query(User).filter_by(name="vasia").first()
+oneOfDept = session.query(Dept)
+
+print("oneOfDept: ",oneOfDept)
+
 
 
 #Сессия внимательно следит за нами. Она, для примера, знает, что Вася был модифицирован:
 print("--")
-print("session.dirty", session.dirty)
+print("session.dirty: ", session.dirty)
 print("--")
 #И что еще пара User’ов ожидают сброса в базу:
 print("--")
-print("session.new",session.new)
+print("session.new: ",session.new)
 print("--")
 
-#Занесения изменений в базу
-#session.commit()
 
-print("vasiaUser: ",vasiaUser)
-print("vasiaUser.id: ",vasiaUser.id)
+#Занесения изменений в базу
+session.commit()
+
+#print("vasiaUser: ",vasiaUser)
+#print("vasiaUser.id: ",vasiaUser.id)
 print("engine: ", engine)
 print("metadata: ", metadata)
 print("Session: ", Session)
 print("session: ", session)
-print("ourUser: ",ourUser)
+#print("ourUser: ",ourUser)
 
-print("vasiaUser.id: ",vasiaUser.id)
 
 print("Вывод данных из таблицы по запросу: ")
 
@@ -77,3 +116,9 @@ k=0
 for id, name, fullname in session.query(User.id, User.name, User.fullname):
     print (k+1, id, name, fullname)
     k=k+1
+
+'''
+for name in session.query(.id, User.name, User.fullname):
+    print (k+1, id, name, fullname)
+    k=k+1
+'''
