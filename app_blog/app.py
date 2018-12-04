f19abc3fd75d6909a1e81f868e4116dfa6dbb7ba
AlexKhvostov <Lihach.ok@gmail.com>
2018-12-05 00:34:11 +0300

    add base.html

--- app_blog/app.py
+++ app_blog/app.py
@@ -3,8 +3,4 @@ from app_blog.config import Configuration
 
 app = Flask(__name__)
 app.config.from_object(Configuration) # app не получает свойства debug
-#print(Configuration.DEBUG)
 app.debug = True
-
-#for i in dir(app.debug):
-#    print(i)
--- app_blog/view.py
+++ app_blog/view.py
@@ -3,5 +3,7 @@ from flask import render_template
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    for i in ('Alex', 'Roma' , 'Tania'):
+        name = i
+    return render_template('index.html', n=name)
 
