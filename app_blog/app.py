47acd5b52e6aa5350180b486707d779e2b139222
AlexKhvostov <Lihach.ok@gmail.com>
2018-12-06 00:55:02 +0300

    add blog, link, db , class

--- app_blog/app.py
+++ app_blog/app.py
@@ -1,6 +1,16 @@
 from flask import Flask
 from app_blog.config import Configuration
 
+from flask_sqlalchemy import SQLAlchemy
+
+
+
+
+
 app = Flask(__name__)
 app.config.from_object(Configuration) # app не получает свойства debug
-app.debug = True
+
+
+db = SQLAlchemy(app)
+
+
--- app_blog/main.py
+++ app_blog/main.py
@@ -1,6 +1,10 @@
 from app_blog.app import app
+from app_blog.app import db
 import app_blog.view
 
+from app_blog.posts.blueprint import posts
+
+app.register_blueprint(posts, url_prefix='/blog')
 
 if __name__ == '__main__':
     app.run()
