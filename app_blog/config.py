aa03bf00e517d6ebd14a68e52a922dcea4736821
AlexKhvostov <Lihach.ok@gmail.com>
2019-07-01 02:08:50 +0300

    01.07 edit/ reset project/ edit port for dubl run

--- app_blog/config.py
+++ app_blog/config.py
@@ -1,5 +1,5 @@
 class Configuration(object):
     DEBUG = True
-    HOST_PORT="5000"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_DATABASE_URI = 'mysql+mysqlconnector://alex:Protokol911#@localhost/Learn'
